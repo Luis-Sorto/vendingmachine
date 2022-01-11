148061d419886227a9218d7347a00122456740c4
Luis Sorto <luis.sorto@focusservices.com>
2022-01-11 08:16:57 -0600

    API call commit - 02 adding loader and simulating delayed server response

--- src/LeftPanel.tsx
+++ src/LeftPanel.tsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 import "./LeftPanel.css"
 import Axios from "axios"
 import Product from './Product'
+import CircularProgress from '@mui/material/CircularProgress'
 
 interface Product{
     id: string,
@@ -13,17 +14,21 @@ interface Product{
 
 const LeftPanel : React.FC = () => {
 
-    const [products, setProducts] = useState<Product[]>([])
+    const [products, setProducts] = useState<Product[]>()
 
     useEffect( () => {
         Axios.get("https://products-api-ten.vercel.app/api")
         .then( (response) => {
-            setProducts(response.data)
+            setTimeout(()=>{
+                setProducts(response.data)
+            }, 3000)
         })
         .catch((error) => {
             console.log(error)
-        })
+        })   
     }, [])
+
+    
 
 
     return (
@@ -33,19 +38,23 @@ const LeftPanel : React.FC = () => {
             </div>
 
             <div className="leftPanel__productContainer">
-                {
+                { products ?
                     products.map((product, index) => {
                         return(
-                            <div className="leftPanel__product">
+                            <div className="leftPanel__product" key = {index}>
                                 <Product 
-                                    key={index}
                                     id = {product.id} 
                                     name = {product.name} 
                                     preparation_time = {product.preparation_time} 
                                     thumbnail = {product.thumbnail} />
                             </div>
                         )   
-                    })
+                    }) : 
+                    (<div className="leftPanel__productContainer__loader">
+                        <CircularProgress color="secondary" />                      
+                        <p >Loading products...</p>
+                    </div>)
+                    
                 }
             </div>
         </div>
